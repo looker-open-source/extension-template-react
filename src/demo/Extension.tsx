5fdccf2489cb490c02828e8f247590d2e8286b24
Bryn Ryans <bryn.ryans@looker.com>
2019-10-25 12:38:29 -0700

    1. Fix manifest (uri -> url) 2. Improve error handling 3. Code styling 4. Little bit of layout improvement 5. Changed callback property to selectLook

--- src/demo/Extension.tsx
+++ src/demo/Extension.tsx
@@ -1,8 +1,8 @@
 import React from "react"
-import { LookList } from "./LookList";
-import { QueryContainer } from "./QueryContainer";
+import { LookList } from "./LookList"
+import { QueryContainer } from "./QueryContainer"
 import { ExtensionContext } from "../framework/ExtensionWrapper"
-import { ILook } from "@looker/sdk";
+import { ILook } from "@looker/sdk"
 import { Switch, Route, RouteComponentProps, withRouter } from "react-router-dom"
 
 interface ExtensionState {
@@ -12,6 +12,7 @@ interface ExtensionState {
   queryResult?: string
   runningQuery: boolean
   loadingLooks: boolean
+  errorMessage?: string
 }
 
 class ExtensionInternal extends React.Component<RouteComponentProps, ExtensionState> {
@@ -38,70 +39,93 @@ class ExtensionInternal extends React.Component<RouteComponentProps, ExtensionSt
     const path: string[] = this.props.location.pathname.split("/")
     if (path.length > 1) {
       const id: number = parseInt(this.props.location.pathname.split("/")[1], 10)
-      this.setState({selectedLookId: id})
+      this.setState({ selectedLookId: id })
       this.runLook(id)
     }
   }
 
   async runLook(look_id: number) {
-    const look = this.state.looks.find(l => l.id == look_id)
+    const look = this.state.looks.find((l) => l.id == look_id)
     // If no matching Look then return
     if (look === undefined) {
       this.setState({
         currentLook: undefined,
-        queryResult: "Unable to load Look.",
+        errorMessage: "Unable to load Look.",
+        queryResult: "",
         runningQuery: false
       })
       return
     }
 
     // Set Page title
-    this.context.extensionSDK.updateTitle(
-      `Look: ${look.title || "unknown"}`
-    )
+    this.context.extensionSDK.updateTitle(`Look: ${look.title || "unknown"}`)
 
-    this.setState({currentLook: look, runningQuery: true})
+    this.setState({ currentLook: look, runningQuery: true, errorMessage: undefined })
 
-    var result = await this.context.coreSDK.ok(
-      this.context.coreSDK.run_look({look_id: look_id, result_format: "json"})
-    )
-
-    this.setState({
-      queryResult: JSON.stringify(result),
-      runningQuery: false
-    })
+    try {
+      var result = await this.context.coreSDK.ok(
+        this.context.coreSDK.run_look({ look_id: look_id, result_format: "json" })
+      )
+      this.setState({
+        queryResult: JSON.stringify(result, undefined, 2),
+        runningQuery: false
+      })
+    } catch (error) {
+      this.setState({
+        queryResult: "",
+        runningQuery: false,
+        errorMessage: "Unable to run look"
+      })
+    }
   }
 
   async loadLooks() {
-    this.setState({loadingLooks: true})
-    var result = await this.context.coreSDK.ok(
-      this.context.coreSDK.all_looks()
-    )
-    this.setState({
-      // Take up to the first 10 looks
-      looks: result.slice(0,9),
-      loadingLooks: false
-    })
+    this.setState({ loadingLooks: true, errorMessage: undefined })
+    try {
+      var result = await this.context.coreSDK.ok(this.context.coreSDK.all_looks())
+      this.setState({
+        // Take up to the first 10 looks
+        looks: result.slice(0, 9),
+        loadingLooks: false
+      })
+    } catch (error) {
+      this.setState({
+        looks: [],
+        loadingLooks: false,
+        errorMessage: "Error loading looks"
+      })
+    }
   }
 
   onLookSelected(look: ILook) {
-    this.props.history.push("/"+look.id)
+    this.props.history.push("/" + look.id)
     if (look.id !== this.state.selectedLookId) {
-      this.setState({selectedLookId: look.id})
+      this.setState({ selectedLookId: look.id })
       this.runLook(look.id!)
     }
   }
 
   render() {
     return (
-      <div>
-        <LookList loading={this.state.loadingLooks} looks={this.state.looks} callback={(look: ILook) => this.onLookSelected(look)}/>
+      <>
+        {this.state.errorMessage && (
+          <div style={{ backgroundColor: "red", color: "white", height: "20px" }}>{this.state.errorMessage}</div>
+        )}
+        <LookList
+          loading={this.state.loadingLooks}
+          looks={this.state.looks}
+          selectLook={(look: ILook) => this.onLookSelected(look)}
+        />
         <Switch>
-          <Route path="/:id">
-            <QueryContainer look={this.state.currentLook} results={this.state.queryResult} running={this.state.runningQuery}/>
+          <Route path='/:id'>
+            <QueryContainer
+              look={this.state.currentLook}
+              results={this.state.queryResult}
+              running={this.state.runningQuery}
+            />
           </Route>
         </Switch>
-      </div>
+      </>
     )
   }
 }
--- src/demo/LookList.tsx
+++ src/demo/LookList.tsx
@@ -1,32 +1,29 @@
 import React from "react"
 import { ILook } from "@looker/sdk"
-import { ListItem } from "./ListItem";
+import { ListItem } from "./ListItem"
 
 interface LookListProps {
-    looks: ILook[]
-    loading: boolean
-    callback: (look: ILook) => void
+  looks: ILook[]
+  loading: boolean
+  selectLook: (look: ILook) => void
 }
 
-export const LookList: React.FC<LookListProps> = (props) => {
-  return (
-    <div style={{width: "auto", float: "left", marginTop: 10}}>
-        <h3>Look List</h3>
-        {
-            props.loading ?
-            (<h4 style={{marginRight: 30}}>Loading...</h4>) :
-            (
-                <ul style={{padding: 10}}>
-                {props.looks.map(look => {
-                    return (
-                        look.id !== undefined ?
-                        (<ListItem key={look.id} id={look.id!} label={look.title!} callback={() => props.callback(look)}/>) :
-                        (<h4 key="error">Failed to load</h4>)
-                    )
-                })}
-            </ul>
-            )
-        }
-    </div>
-  )
-}
+export const LookList: React.FC<LookListProps> = (props) => (
+  <div style={{ width: "auto", float: "left", margin: 10 }}>
+    <h3>Look List</h3>
+
+    {props.loading ? (
+      <h4 style={{ marginRight: 30 }}>Loading...</h4>
+    ) : (
+      <ul style={{ padding: 10 }}>
+        {props.looks.map((look) => {
+          return look.id !== undefined ? (
+            <ListItem key={look.id} id={look.id!} label={look.title!} callback={() => props.selectLook(look)} />
+          ) : (
+            <h4 key='error'>Failed to load</h4>
+          )
+        })}
+      </ul>
+    )}
+  </div>
+)
