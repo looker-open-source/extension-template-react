db64473580359557867cfddd319fa21c5ed8cba8
Bryn Ryans <bryn.ryans@looker.com>
2019-10-29 17:15:33 -0700

    1. Add looker open source extensions packages 2. Remove embedded ExtensionWrapper 3. Replace ExtensionWrapper with ExtensionProvider 4. License format

--- src/demo/Extension.tsx
+++ src/demo/Extension.tsx
@@ -1,29 +1,31 @@
-// The MIT License (MIT)
-
-// Copyright (c) 2019 Looker Data Sciences, Inc.
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Looker Data Sciences, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import React from "react"
 import { LookList } from "./LookList"
 import { QueryContainer } from "./QueryContainer"
-import { ExtensionContext } from "../framework/ExtensionWrapper"
+import { ExtensionContext } from "@looker/extension-sdk-react"
 import { ILook } from "@looker/sdk"
 import { Switch, Route, RouteComponentProps, withRouter } from "react-router-dom"
 
@@ -65,7 +67,7 @@ class ExtensionInternal extends React.Component<RouteComponentProps, ExtensionSt
       this.runLook(id)
     }
   }
-  
+
   /*
   // TEMPLATE CODE FOR RUNNING ANY QUERY
   async runQuery() {
@@ -94,7 +96,7 @@ class ExtensionInternal extends React.Component<RouteComponentProps, ExtensionSt
         errorMessage: "Unable to run query"
       })
     }
-  
+
   */
 
   async runLook(look_id: number) {
